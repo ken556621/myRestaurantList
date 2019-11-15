2328d44a0760b983d446f026f90af35abda82238
ken <ken556621@gmail.com>
2019-11-15 12:49:18 +0800

    Add alert function & modify models

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const restaurantSchema = new Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     name:{
         type: String,
         required: true
